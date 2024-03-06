4d9a7c962835466d20cbc3ab978bad0ac0ea69d6
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-06 16:22:20 +0530

    Delete hello_world/target directory

--- hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d
+++
@@ -1,5 +0,0 @@
-/Users/mranv/Desktop/rust-projects/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643: src/main.rs
-
-/Users/mranv/Desktop/rust-projects/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/mranv/Desktop/rust-projects/hello_world/target/debug/hello_world: /Users/mranv/Desktop/rust-projects/hello_world/src/main.rs
