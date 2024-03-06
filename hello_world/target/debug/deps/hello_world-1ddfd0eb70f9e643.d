d53bf807f704d9bfaf458265ab534bee7b9cb808
mranv <iamanubhavgain@gmail.com>
2024-03-06 14:19:03 +0530

    Added: Hello Program

---
+++ hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d
@@ -0,0 +1,5 @@
+/Users/mranv/Desktop/rust-projects/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643: src/main.rs
+
+/Users/mranv/Desktop/rust-projects/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/mranv/Desktop/rust-projects/hello_world/target/debug/hello_world: /Users/mranv/Desktop/rust-projects/hello_world/src/main.rs
